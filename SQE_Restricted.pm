7676cc65cee4403d20f45c86a6a973c85ca4c3bd
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-02-10 22:31:48 +0100

    Let default SQE_Requsted through.

---
+++ SQE_Restricted.pm
@@ -0,0 +1,11 @@
+package SQE_Restricted;
+use strict;
+use warnings FATAL => 'all';
+
+use constant {
+    DSN_SQE =>   "dbi:mysql:SQE:127.0.0.1:3307",
+    DB_USERNAME => 'root',
+    DB_PASSWORD => 'none',
+};
+
+1;
