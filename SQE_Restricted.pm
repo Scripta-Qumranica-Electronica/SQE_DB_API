b46f6b0a098365414bca72c020eea63c5ca68a4b
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-07-18 16:21:44 +0200

    New Database Docker connection.

--- SQE_Restricted.pm
+++ SQE_Restricted.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings FATAL => 'all';
 
 use constant {
-    DSN_SQE =>   "dbi:mysql:SQE_DEV:127.0.0.1:3307",
+    DSN_SQE =>   "dbi:mysql:SQE_DEV:SQE_Database:3306",
     DB_USERNAME => 'root',
     DB_PASSWORD => 'none',
 };
